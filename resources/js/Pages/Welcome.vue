47ed8ee3040e6e925c9082838fd9f12b1c4c8ead
JuswaOof <joshofficialxxx@gmail.com>
2026-09-17 20:35:10 +0800

    feat: set up vue inertia and tailwind

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex min-h-screen items-center justify-center bg-gray-100">
+        <h1 class="rounded-xl bg-blue-600 px-8 py-4 text-4xl font-bold text-white shadow-lg">
+            Hello from TindahanOS!
+        </h1>
+    </div>
+</template>
